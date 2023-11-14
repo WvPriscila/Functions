7d66728f9cc12a58bf5e3b5deb55a09d6a92ca5c
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-13 22:35:21 -0300

    Create mutação.py

---
+++ mutação.py
@@ -0,0 +1,40 @@
+
+
+def Possivel_mutação(individua):
+    for i in range(len(individua)):
+        if random.random() < taxa_mutacao:
+            individua[i] = random.randint(0, 9)
+    return individua
+
+
+def mutação_Obrigatória(individua):
+    for i in range(len(individua)):
+        individua[i] = random.randint(0, 9)
+    return individua
+
+
+def Possivel_mutação(individua):
+    for i in range(len(individua)):
+        if random.random() < taxa_mutacao:
+            individua[i] = random.randint(0, 9)
+    return individua
+
+
+def mutação_Obrigatória(individua):
+    for i in range(len(individua)):
+        individua[i] = random.randint(0, 9)
+    return individua
+
+
+
+
+
+
+# Função para realizar a mutação em um indivíduo
+def mutacao(individuo):
+    for i in range(num_genes):
+        if random.random() < taxa_mutacao:
+            individuo[i] = 1 - individuo[i]
+
+
+
