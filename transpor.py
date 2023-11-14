5e516387b000d097cece380feff9980717744166
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-13 22:30:47 -0300

    Update transpor.py

--- transpor.py
+++ transpor.py
@@ -16,3 +16,8 @@ def transpor_matriz(matriz):
             matriz_transposta[p].append(matriz[i][p])
 
     return matriz_transposta
+
+# Exemplo de uso
+A = [[1, 2, 3,4,5], [ 6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20],[21,22,23,24,25]]
+B = transpor_matriz(A)
+print(B)
