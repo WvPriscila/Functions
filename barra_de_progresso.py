38d9d164d8c70d1522cbb92473785c94e72a11e1
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-15 14:15:39 -0300

    Update barra_de_progresso.py

--- barra_de_progresso.py
+++ barra_de_progresso.py
@@ -7,3 +7,15 @@ def exibir_barra_de_progresso(Ga, Gf):
             time.sleep(0.2)  # 0.2 segundos (200 milissegundos) para cada caractere
 
         break
+
+
+def exibir_barra_de_progresso(Ga,Gf):
+    caracteres = ['|', '/', '-', '\\']
+    while True:
+        for char in caracteres:
+            sys.stdout.write(f'\r{char}')
+            sys.stdout.flush()
+            time.sleep(0.2)  # 0.1 segundos (10 centésimos) para cada caractere
+        print(f'Porcentagem:   {100*(Ga/Gf)}\n')
+        if Ga >= Gf:
+            break
