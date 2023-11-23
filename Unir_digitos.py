863aa4f27d19736d0925b484993b4746ea667804
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-23 15:28:13 -0300

    Update Unir_digitos.py

--- Unir_digitos.py
+++ Unir_digitos.py
@@ -1,12 +1,17 @@
-def Unir_digitos(lista):
+def Unir(individua):
+        
+    if type(individua[0]) == type(Decimal(9)):
+        individua = individua[1:]
+
+    
     A = ''
-    # print("lista",lista)
-    sinal = int(lista[0])
-    for i in lista:
+    sinal = int(individua[0])
+    for i in individua:
         i = str(i)
         A = A + i
+    
 
-    B = f"{A[1:digitos_inteiro + 1]}.{A[digitos_inteiro + 1:]}"
-
-    número = (-1) ** sinal * Decimal(B)
+    B = f"{A[:digitos_inteiro]}.{A[digitos_inteiro :]}"
+    B = Decimal(B)
+    número = (-1) ** sinal * B
     return número
