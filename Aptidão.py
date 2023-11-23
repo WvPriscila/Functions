353f3cfe8f0bd23f915019c26c9fd8105dbbf356
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-23 15:27:28 -0300

    Update Aptidão.py

--- Aptidão.py
+++ Aptidão.py
@@ -1,12 +1,36 @@
-def Exempla(n):
-    n = n**2
+def Aptidão(individua):
+    
+    if type(individua[0]) == type(Decimal(9)):
+        individua = individua[1:]
+    
+    
+    A = ''
+    sinal = int(individua[0])
+    individua = individua[1:]
+    
+    for i in individua:
+        i = str(i)
+        A = A + i
+    
+    B = f"{A[:digitos_inteiro]}.{A[digitos_inteiro :]}"
+    B = Decimal(B)
+    número = (-1) ** sinal * B
+    valor =  número ** 2
 
-def Aptdão(individua):
+    diferenca = Decimal(valor - objetivo)  
+    
+    if diferenca < 0:
+        diferenca = -diferenca
+        
+ 
+    if diferenca == 0:
+        popy.append(individuo)
+        if individuo in POPULAÇÃO:
+            POPULAÇÃO.remove(individuo)
+            individuo.insert( 0,"A" )
+            return individuo
 
-    individua = Unir_digitos(individua)
-    Valor = Exempla(individua)
-
-    Apt = 1/(Valor--1)
-
-    return Apt
-
+    
+    else:
+        aptidao = Decimal(1 / diferenca)
+        return aptidao/Decimal(1E-10)
