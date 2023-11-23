54ce5b1e0221c38f24e8235d4ed8240626b41ac2
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-23 15:32:57 -0300

    Update inicializar_populacao.py

--- inicializar_populacao.py
+++ inicializar_populacao.py
@@ -3,3 +3,14 @@ def inicializar_populacao():
         individua = [random.randint(0, 9) for i in range(num_genes)]
         POPULAÇÃO.append(individua)
     return POPULAÇÃO
+
+
+def inicializar_populacao(POPULAÇÃO):
+    
+    POP = []
+    for i in range(tamanho_população_inicial):
+        individuo = [random.randint(0, 9) for i in range(num_genes)]
+        individuo.insert(0,Aptidão(individuo))
+        POP.append(individuo)
+        
+    return POP
