567516bf2b24edb95d014265d07b3c996e26aa48
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-27 20:26:45 -0300

    Update Variável_dinamica.py

--- Variável_dinamica.py
+++ Variável_dinamica.py
@@ -3,18 +3,14 @@ for n in range(0, 7):
 
 for p in range(0, 7):
     globals()[f'variável{p}'] = f"Olá o número da variável é:  {p}!"
-
-
+    
 print(Var0)
 print(variável0)
 
 #output
 """
-
         WW
         Olá o número da variável é:  0!
-
-
 
 """
 #-----------------------------------------------------------------------------#
