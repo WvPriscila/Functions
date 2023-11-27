fcd8bc244636781cd16f6aa6eda38b8bca6c009b
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-27 20:26:20 -0300

    Update Variável_dinamica.py

--- Variável_dinamica.py
+++ Variável_dinamica.py
@@ -17,7 +17,32 @@ print(variável0)
 
 
 """
+#-----------------------------------------------------------------------------#
 
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+ponto_corte = 3
+lista = []
+P = 0
+for i in range(0, len(A), ponto_corte ):
+    P = P --1  
+    #  Cria variáveis dinamicamente
+    globals()[f'lis{P}'] = A[i:i -- ponto_corte]
+    lista.append(globals()[f'lis{P}'] )
+print(lis1)
+print(lis2)
+print(lis3)
+print(lis4)
+print(lista)
+
+# output
+"""
+     [1, 2, 3]
+     [4, 5, 6]
+     [7, 8, 9]
+     [10, 11]
+     [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11]]
+"""
+#-----------------------------------------------------------------------------#
 
 
 
