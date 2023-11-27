37b83d920190dbc13c19f03f002e477c44701d21
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-27 19:34:00 -0300

    Update corte.py

--- corte.py
+++ corte.py
@@ -10,18 +10,30 @@ for i in range(0, len(A) - (len(A)%ponto_corte), ponto_corte):
 
 lista.append(resto_lista)
 print(lista)
+
+# output
+"""[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11]]"""
+
+
 #------------------------------------------------------------------------------#
 
-""" inversa"""
-B = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
-ponto_corte = 6
+A = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+ponto_corte = 3
 
-for i in range(0, len(B), len(B) + (len(B)%ponto_corte)):
-    print(B[i:i + len(B) - ponto_corte])
+for i in range(0, len(A), len(A) + (len(A)%ponto_corte)):
+    print(A[i:i + len(A) - ponto_corte])
 
 
-resto_lista =B[-(len(B)-(len(B)%ponto_corte)):] 
+resto_lista =A[-(len(A)-(len(A)%ponto_corte)):] 
 print(resto_lista)
+
+# output
+""" 
+    [12, 13, 14, 15, 16, 17, 18, 19]
+    [14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+"""
+
 
 #------------------------------------------------------------------------------#
 
@@ -42,6 +54,15 @@ for i in range(0, len(A), tamanho_sublista):
 # Imprima as sublistas
 for sublista in sublistas:
     print(sublista)
+
+
+#output 
+""" 
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+
+"""
 #------------------------------------------------------------------------------#
 
 A = [1,2,3,4,5,6,7,8,9,10,11]
@@ -53,3 +74,6 @@ for i in range(0, len(A), ponto_Corte):
     sublista = A[i:i + ponto_Corte]
     sublistas.append(sublista)
 print(sublistas)
+
+# output
+""" [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11]] """
