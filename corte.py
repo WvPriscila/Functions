aaabd064234ece00680ca250bb5f29f1b7b959a0
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-27 01:39:46 -0300

    Update corte.py

--- corte.py
+++ corte.py
@@ -1,12 +1,10 @@
 A = [1,2,3,4,5,6,7,8,9,10,11]
-
 ponto_corte = 3
 
 resto_lista = A[-(len(A)%ponto_corte):]
 
 lista= []   
     
-
 for i in range(0, len(A) - (len(A)%ponto_corte), ponto_corte):
     lista.append(A[i:i + ponto_corte])
 
@@ -17,14 +15,8 @@ print(lista)
 #------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------#
 
-
 A = [1,2,3,4,5,6,7,8,9,10,11]
 ponto_Corte = 3
-
-numero_gene_perfeitamente_cortados = len(A) // ponto_Corte
-numero_gene_imperfeitamente_cortado = len(A) % ponto_Corte
-
-p = len(A) - (len(A) // ponto_Corte)
 
 sublistas = []
 
