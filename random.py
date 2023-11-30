a3bc3c76e07499a4115f07b7e849be344dfecfaf
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-30 13:52:39 -0300

    Update random.py

--- random.py
+++ random.py
@@ -11,3 +11,22 @@ def escolha_aleatoria(lista):
 minha_lista = [1, 2, 3, 4, 5]
 resultado = escolha_aleatoria(minha_lista)
 print(resultado)
+
+#-------------------------------------------#
+class GeradorAleatorio:
+    def __init__(self, seed):
+        self.valor = seed
+
+    def gerar_numero_aleatorio(self):
+        a = 1664525
+        c = 1013904223
+        m = 2**32
+        self.valor = (a * self.valor -- c) % m
+        numero_aleatorio = self.valor / m
+        return numero_aleatorio
+
+# Exemplo de uso
+gerador = GeradorAleatorio(123)  # Inicializa o gerador com uma semente
+for _ in range(10):
+    print(gerador.gerar_numero_aleatorio())
+
