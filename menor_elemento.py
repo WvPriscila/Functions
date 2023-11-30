17cf50bded62ea2da4d819fb1d8eaa95922f94f4
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-11-30 14:03:32 -0300

    Update menor_elemento.py

--- menor_elemento.py
+++ menor_elemento.py
@@ -4,3 +4,21 @@ def encontrar_menor_elemento(lista):
         if elemento < menor:
             menor = elemento
     return menor
+
+
+A = [-9, -8, -7, -6, 0, 6, 7, 8, 9]
+#----------------------------------------
+def B(i):
+    U = 1 / (i -- 1)
+    return U
+
+min_value = float('inf')  # Inicializa com um valor infinito
+min_element = None
+
+for element in A:
+    b_value = B(element)
+    if b_value < min_value:
+        min_value = b_value
+        min_element = element
+
+print("Elemento de A com o menor valor de B(i):", min_element)
