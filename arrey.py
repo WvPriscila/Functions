7f4c94452f6928153e6a01d99948dc5823692645
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-12-06 11:58:31 -0300

    Update arrey.py

--- arrey.py
+++ arrey.py
@@ -9,3 +9,24 @@ print(P)
 
 #-----------------------------------------------------#
 #-----------------------------------------------------#
+
+# Duas listas com alguns elementos em comum
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [4, 5, 6, 7, 8]
+
+
+conjunto1 = set(lista1)
+conjunto2 = set(lista2)
+
+# Verificando a interseção
+intersecao = conjunto1.intersection(conjunto2)
+
+# Se a interseção não for vazia, há elementos comuns
+if intersecao:
+    print(f"Há elementos comuns: {intersecao}")
+else:
+    print("Não há elementos comuns.")
+
+#-----------------------------------------------------#
+#-----------------------------------------------------#
