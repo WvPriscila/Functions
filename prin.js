930555ec81e69ffbce64d4f1e09ef29e683d1af6
WvPriscila <68048874+WvPriscila@users.noreply.github.com>
2023-12-21 16:12:04 -0300

    Create prin.js

---
+++ prin.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aqui é o título do site</title>
+</head>
+<body>
+
+<script>
+    document.write("oi")
+</script>
+
+</body>
+</html>
+//-----------------------------------------------------------------------------------------------//
+
+<!DOCTYPE html>
+<html lang = " pt-br">
+<head>
+      <meta charset = "UTF-8">
+	  <meta http-equiv="viewport" coment="width=device-width, initial-scale=1.0">
+	  <title> aqui é o título do site</title>
+</head>
+<body>
+
+
+<script src = "A.js "> </script>
+
+</body>
+</html>
+
